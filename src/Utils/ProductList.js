bfcbe883929d98639db7723fce25d37032d1fcde
Himanshu Sonkar <himanshusonkar84@gmail.com>
2023-08-12 01:56:10 +0530

    Create ProductList.js

---
+++ src/Utils/ProductList.js
@@ -0,0 +1,191 @@
+import catOne from "../Assets/products/catOne.png";
+import catTwo from "../Assets/products/catTwo.png";
+import catThree from "../Assets/products/catThree.png";
+import catFour from "../Assets/products/catFour.png";
+import catFive from "../Assets/products/catFive.png";
+import catSix from "../Assets/products/catSix.png";
+import catSeven from "../Assets/products/catSeven.png";
+import catEight from "../Assets/products/catEight.png";
+import catNine from "../Assets/products/catNine.png";
+import catTen from "../Assets/products/catTen.png";
+import catEleven from "../Assets/products/catEleven.png";
+import catTwelve from "../Assets/products/catTwelve.png";
+import pro11 from "../Assets/home/updated/homeProOne.jpg";
+import pro12 from "../Assets/home/updated/homeProThree.jpg";
+import pro21 from "../Assets/products/updated/pro21.jpg";
+import pro22 from "../Assets/products/updated/pro22.jpg";
+import pro41 from "../Assets/products/updated/pro41.jpg";
+import pro42 from "../Assets/products/updated/pro42.jpg";
+import pro43 from "../Assets/products/updated/pro43.jpg";
+import pro44 from "../Assets/products/updated/pro44.jpg";
+import pro45 from "../Assets/products/updated/pro45.jpg";
+import pro46 from "../Assets/products/updated/pro46.jpg";
+import pro61 from "../Assets/products/updated/pro61.jpg";
+import pro62 from "../Assets/products/updated/pro62.jpg";
+import pro63 from "../Assets/products/updated/pro63.jpg";
+import pro64 from "../Assets/products/updated/pro64.jpg";
+import pro65 from "../Assets/products/updated/pro65.jpg";
+import pro66 from "../Assets/products/updated/pro66.jpg";
+import pro81 from "../Assets/products/updated/pro81.jpg";
+import pro82 from "../Assets/products/updated/pro82.jpg";
+import pro83 from "../Assets/products/updated/pro83.jpg";
+import pro84 from "../Assets/products/updated/pro84.jpg";
+import pro85 from "../Assets/products/updated/pro85.jpg";
+import pro91 from "../Assets/products/updated/pro91.jpg";
+import pro92 from "../Assets/products/updated/pro92.jpg";
+import pro93 from "../Assets/products/updated/pro93.jpg";
+import pro94 from "../Assets/products/updated/pro94.jpg";
+import pro95 from "../Assets/products/updated/pro95.jpg";
+import proE1 from "../Assets/products/updated/proE1.jpg";
+import proE2 from "../Assets/products/updated/proE2.jpg";
+import proE3 from "../Assets/products/updated/proE3.jpg";
+import proE4 from "../Assets/products/updated/proE4.jpg";
+import proT1 from "../Assets/products/updated/proT1.jpg";
+import proT2 from "../Assets/products/updated/proT2.jpg";
+import proT3 from "../Assets/products/updated/proT3.jpg";
+import proT4 from "../Assets/products/updated/proT4.jpg";
+import proT5 from "../Assets/products/updated/proT5.jpg";
+
+// import pro from '../Assets/products/updated';
+const ProductList = [
+  {
+    images: [pro11, pro12, pro11, pro12, pro11],
+    heading: "Laminated Food/Nonfood Packaging Pouches",
+    description:
+      "Our laminated food packaging pouches are pouches with 50 to 120 micron thickness and are available in all colors and sizes! With an attractive glossy finish and customized design, they are sure to catch anybody’s attention! These are 2-layer/3-layer printed pouches that offer double safety from leakage and air gaps, ensuring freshness and aroma are retained in the pouch!  ",
+    // " Our laminated food packaging pouches are pouches with" +
+    // "50-to-120-micron thickness and are available in all colors" +
+    // "and sizes! With an attractive glossy finish and customized" +
+    // "design, they are sure to catch anybody’s attention! " +
+    // "These are 2-layer/3-layer printed pouches that offer double" +
+    // "safety from leakage and air gaps, ensuring freshness and" +
+    // "aroma are retained in the pouch! ",
+  },
+
+  {
+    images: [pro21, pro22, pro21, pro22, pro21],
+    heading: "Clear Vacuum Pouches",
+    description:
+      "These customized vacuum pouches are long-lasting, durable  and of high quality! They come in customized sizes and transparent. ",
+    // "These customized vacuum pouches are long-lasting, durable  " +
+    // "and of high quality! They come in customized sizes and are" +
+    // "transparent.",
+  },
+
+  {
+    images: [catThree, catThree, catThree, catThree, catThree],
+    heading: "2 and 3 Layer printed and Non-printed Laminated Rolls",
+    description:
+      "These printed 2 and 3-layer laminated packaging rolls that are reliable and of high quality! They are disposable and eco-friendly with a thickness of 30 Micron to 150micronsdepending upon the packaging need. It is essential to have good finishing in packaging rolls, especially food packaging and we understand this very accurately!  These laminated Packaging Rolls are made with utmost care and premium quality! These pouches are weatherproof and lightweight.",
+    // "  These printed 2 and 3-layer laminated packaging rolls that" +
+    // "are reliable and of high quality! They are disposable and" +
+    // "eco-friendly with a thickness of 30 Micron to" +
+    // "150micronsdepending upon the packaging need." +
+    // "It is essential to have good finishing in packaging rolls," +
+    // "especially food packaging and we understand this very" +
+    // "accurately!  These laminated Packaging Rolls are made" +
+    // "with utmost care and premium quality! These pouches are" +
+    // "weatherproof, lightweight and known for corrosion" +
+    // "resistance!  ",
+  },
+
+  {
+    images: [pro41, pro42, pro43, pro44, pro45],
+    heading: "Stand –Up Pouches",
+    description:
+      "These pouches are extremely attractive and stand well on the shelf display and used to pack many products like coffee, nuts, and snacks. They are of food grade and come in any color and customized design! These pouches come in 2 and 3 layers. ",
+    // " These pouches are extremely attractive and stand well on the" +
+    // "shelf display and used to pack many products like coffee," +
+    // "nuts, and snacks. They are of food grade and come in any" +
+    // "color and customized design! These pouches come in 2 and" +
+    // "3 layers. ",
+  },
+
+  {
+    images: [pro46, pro42, pro43, pro44, pro45],
+    heading: "Printed Stand-Up Pouches",
+    description:
+      "We use the Rotogravure printing technique to make these disposable stand-up pouches that come in attractive colors and sizes! The zip closure ensures aroma of the product is intact!Packing spices requires multilayer lamination to keep the aroma and freshness intact and we ensure that with our superior quality stand-up pouch! We offer customized thickness and design, so your wish is our command!",
+    // " We use the Rotogravure printing technique to make these" +
+    // "disposable stand-up pouches that come in attractive colors" +
+    // "and sizes! The zip closure ensures aroma of the product is" +
+    // "intact!Packing spices requires multilayer lamination to keep" +
+    // "the aroma and freshness intact and we ensure that with our" +
+    // "superior quality stand-up pouch! We offer customized" +
+    // "thickness and design, so your wish is our command!  ",
+  },
+
+  {
+    images: [pro61, pro62, pro63, pro64, pro65],
+    heading: "LDPE Pouches",
+    description:
+      " To cater to a wide range of customers and meet the increasing demand, we have two types of LDPE Pouches, printed and plain.",
+    //  ' " To cater to a wide range of customers and meet the" +
+    //   "increasing demand, we have two types of LDPE Pouches," +
+    //   "printed and plain.",'
+  },
+
+  {
+    images: [pro65, pro62, pro63, pro64, pro61],
+    heading: "Plain LDPE Pouch",
+    description:
+      "These transparent plain LDPE pouches are waterproof are great for international packing! They come in various sizes with 40micron to 150micron thickness.",
+    // " These transparent plain LDPE pouches are waterproof are" +
+    // "great for international packing! They come in various sizes" +
+    // "with 40micron to 150micron thickness.  ",
+  },
+
+  {
+    images: [pro81, pro82, pro83, pro84, pro85],
+    heading: "Three Side Sealed Pouch",
+    description:
+      "Being pioneers in the packaging industry, we know the importance of food packaging thoroughly. These pouches sealed neatly from three sides are lightweight, durable and used widely for packaging! They are flexible and easy to pack. ",
+    // " Being pioneers in the packaging industry, we know the" +
+    // "importance of food packaging thoroughly. These pouches" +
+    // "sealed neatly from three sides are lightweight, durable and" +
+    // "used widely for packaging! They are flexible and easy to" +
+    // "pack.  ",
+  },
+  {
+    images: [pro91, pro92, pro93, pro94, pro95],
+    heading: "Centre Seal Pouches for Food Packaging",
+    description:
+      "These pouches are ideal for packaging powders and food grain and ensure moisture-proof protection and leakage protection with disposable feature!",
+    // "  These pouches are ideal for packaging powders and food grain" +
+    // "and ensure moisture-proof protection and leakage protection" +
+    // "with disposable feature! ",
+  },
+  {
+    images: [catTen, catTen, catTen, catTen, catTen],
+    heading: "Metalized Pouches or Rolls",
+    description:
+      "Using polyester film, these metalized pouches ensure the heat barrier is adequate and the product remains fresh, crisp and waterproof! They are recyclable too!",
+    // "  Using polyester film, these metalized pouches ensure the" +
+    // "heat barrier is adequate and the product remains fresh," +
+    // "crisp and waterproof! They are recyclable too! ",
+  },
+  {
+    images: [proE1, proE2, proE3, proE4, proE2],
+    heading: "Incense Stick Pouch",
+    description:
+      " The essence of Incense stick is in its fragrance that adds to the divinity is any religious ritual! Thus, we ensure that this fragrance stays intact with our PET pouches that are heat sealed and have three-layer lamination! These are available in all colors with a matte finish to add to the elegance! ",
+    // " The essence of Incense stick is in its fragrance that adds" +
+    // "to the divinity is any religious ritual! Thus, we ensure" +
+    // "that this fragrance stays intact with our PET pouches that" +
+    // "are heat sealed and have three-layer lamination! These are" +
+    // "available in all colors with a matte finish to add to the" +
+    // "elegance!  ",
+  },
+  {
+    images: [proT1, proT2, proT3, proT4, proT5],
+    heading: "Matte Finish Pouch",
+    description:
+      "Matte Finish has an elegance and suitable for some niche products!! They do not blind during sunshine and are easy to recognize and read! Our metalized matte finish pouches have customized color and are a sight to behold! ",
+    // " Matte Finish has an elegance and suitable for some niche" +
+    // "products!! They do not blind during sunshine and are easy to" +
+    // "recognize and read! Our metalized matte finish pouches have" +
+    // "customized color and are a sight to behold!  ",
+  },
+];
+
+export default ProductList;
